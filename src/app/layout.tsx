442ef6819269ca1ed687e086300d246e77ee8ddc
dzmitrybaranau <dimabaranov98@gmail.com>
2024-07-17 11:41:07 +0300

    feat(web-app-ready)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import './global.css'
+import "./global.css";
+import WebAppReady from "@/components/WebAppReady/WebAppReady";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <WebAppReady />
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/components/WebAppReady/WebAppReady.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import WebApp from "@twa-dev/sdk";
+
+import React, { useEffect } from "react";
+
+export interface IWebAppReadyProps {}
+
+/**
+ * Web App Ready handler
+ */
+function WebAppReady(props: IWebAppReadyProps) {
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      WebApp.ready();
+    }
+  }, []);
+  return <div> </div>;
+}
+
+export default WebAppReady;
