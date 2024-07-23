48ccfeeb2f4eb03258ce7f51faff600e945d6767
dzmitrybaranau <dimabaranov98@gmail.com>
2024-07-23 12:56:42 +0300

    feat(add-crypto)

--- src/components/AddTokenModal/AddTokenModal.tsx
+++ src/components/AddTokenModal/AddTokenModal.tsx
@@ -6,6 +6,8 @@ import styles from "./AddTokenModal.module.scss";
 import Input from "@/components/Input/Input";
 import inputStyles from "../Input/Input.module.scss";
 import Button from "@/components/TotalBalance/components/Button/Button";
+import { AnimatePresence, motion } from "framer-motion";
+import { useUserStore } from "@/store";
 
 export interface IAddTokenModalProps {}
 
@@ -26,11 +28,10 @@ interface IFormInterface {
  */
 function AddTokenModal(props: IAddTokenModalProps) {
   const [isBuyOpen, setIsBuyOpen] = useState(false);
+  const buyToken = useUserStore((state) => state.buyToken);
+  const userAccount = useUserStore((state) => state.userAccount);
 
-  const handleOpenBuy = () => {
-    console.log("OPEN");
-    setIsBuyOpen(true);
-  };
+  const handleOpenBuy = () => setIsBuyOpen(true);
   const handleCloseBuy = () => setIsBuyOpen(false);
 
   const [formState, setFormState] = useState<IFormInterface>({
@@ -39,6 +40,7 @@ function AddTokenModal(props: IAddTokenModalProps) {
     amount: undefined,
     price: undefined,
   });
+  const { token, usdt, amount, price } = formState;
 
   const [autoCalculate, setAutoCalculate] = useState(true);
 
@@ -48,12 +50,29 @@ function AddTokenModal(props: IAddTokenModalProps) {
       ...prevState,
       [id]: value,
     }));
-
     if (id === "usdt" || id === "amount") {
       setAutoCalculate(true);
     } else if (id === "price") {
       setAutoCalculate(false);
     }
+  };
+
+  const handleAddToBagClick = () => {
+    console.log("BUY");
+    buyToken({
+      tokenId: token?.value as string,
+      amount: amount as string,
+      usdt: usdt as string,
+      price: price as string,
+      date: new Date().toISOString(),
+    });
+    setIsBuyOpen(false);
+    setFormState({
+      token: undefined,
+      usdt: undefined,
+      amount: undefined,
+      price: undefined,
+    });
   };
 
   useEffect(() => {
@@ -77,6 +96,8 @@ function AddTokenModal(props: IAddTokenModalProps) {
     { value: "LTC", label: "LTC" },
   ];
 
+  console.log({ userAccount });
+
   return (
     <>
       <Button
@@ -92,106 +113,118 @@ function AddTokenModal(props: IAddTokenModalProps) {
       >
         + Buy
       </Button>
-      {isBuyOpen && (
-        <div className={styles.root}>
-          <button className={styles.closeButton} onClick={handleCloseBuy} />
-          <form onChange={handleFormChange}>
-            <div className={styles.inputWrapper}>
-              <label className={inputStyles.label}>Token</label>
-              <Select
-                styles={{
-                  container: (base, props) => ({
-                    ...base,
-                    marginTop: 8,
-                    outline: "none",
-                  }),
-                  menu: (base, state) => ({
-                    ...base,
-                    background: "#FFF6D6",
-                    padding: "0px",
-                    borderRadius: 8,
-                  }),
-                  menuList: (base, state) => ({
-                    ...base,
-                    background: "#FFF6D6",
-                    padding: "0px",
-                    borderRadius: 8,
-                  }),
-                  option: (base, props) => ({
-                    ...base,
-                    display: "flex",
-                    alignItems: "center",
-                    color: "#292929",
-                    fontWeight: 500,
-                    fontSize: "20px",
-                    background: props.isSelected
-                      ? "#c2baa1"
-                      : props.isFocused
-                        ? "#eee4cb"
-                        : "transparent",
-                    height: "50px",
-                    cursor: "pointer",
-                  }),
-                  control: (base, props) => ({
-                    ...base,
-                    height: 60,
-                    background: "#FFF6D6",
-                    boxShadow: "inset 0px -2px 0px #7C4C27",
-                    border: "1px solid #7C4C27",
-                    borderRadius: "16px",
-                    padding: "0px 8px 0px 8px",
-                    fontSize: "20px",
-                    fontWeight: 500,
-                  }),
-                }}
-                defaultValue={formState.token}
-                onChange={(option) =>
-                  setFormState({ ...formState, token: option || undefined })
-                }
-                options={options}
-              />
-            </div>
-            <div className={styles.inputWrapper}>
-              <Input
-                label="USDT ($)"
-                type="number"
-                id="usdt"
-                value={formState.usdt}
-              />
-            </div>
-            <div className={styles.inputWrapper}>
-              <Input
-                label="Amount"
-                type="number"
-                id="amount"
-                value={formState.amount}
-              />
-            </div>
-            <div className={styles.inputWrapper}>
-              <Input
-                label="Price"
-                type="number"
-                id="price"
-                value={formState.price}
-              />
-            </div>
-          </form>
-          <Button
-            cssProps={{
-              height: "60px",
-              background: "#FED521",
-              fontSize: "24px",
-              fontWeight: 600,
-              color: "#292929",
-              boxShadow:
-                "inset 0px -4px 2px #feb321, 0px 2px 2px rgba(169, 0,0,0.5)",
-              marginTop: 32,
+      <AnimatePresence>
+        {isBuyOpen && (
+          <motion.div
+            initial={{
+              top: "100%",
             }}
+            animate={{ top: "20px" }}
+            exit={{ top: "100%" }}
+            transition={{ ease: "easeInOut" }}
+            className={styles.root}
           >
-            Add To Bag
-          </Button>
-        </div>
-      )}
+            <button className={styles.closeButton} onClick={handleCloseBuy} />
+            <form onChange={handleFormChange}>
+              <div className={styles.inputWrapper}>
+                <label className={inputStyles.label}>Token</label>
+                <Select
+                  styles={{
+                    container: (base, props) => ({
+                      ...base,
+                      marginTop: 8,
+                      outline: "none",
+                    }),
+                    menu: (base, state) => ({
+                      ...base,
+                      background: "#FFF6D6",
+                      padding: "0px",
+                      borderRadius: 8,
+                    }),
+                    menuList: (base, state) => ({
+                      ...base,
+                      background: "#FFF6D6",
+                      padding: "0px",
+                      borderRadius: 8,
+                    }),
+                    option: (base, props) => ({
+                      ...base,
+                      display: "flex",
+                      alignItems: "center",
+                      color: "#292929",
+                      fontWeight: 500,
+                      fontSize: "20px",
+                      background: props.isSelected
+                        ? "#c2baa1"
+                        : props.isFocused
+                          ? "#eee4cb"
+                          : "transparent",
+                      height: "50px",
+                      cursor: "pointer",
+                    }),
+                    control: (base, props) => ({
+                      ...base,
+                      height: 60,
+                      background: "#FFF6D6",
+                      boxShadow: "inset 0px -2px 0px #7C4C27",
+                      border: "1px solid #7C4C27",
+                      borderRadius: "16px",
+                      padding: "0px 8px 0px 8px",
+                      fontSize: "20px",
+                      fontWeight: 500,
+                    }),
+                  }}
+                  defaultValue={formState.token}
+                  onChange={(option) =>
+                    setFormState({ ...formState, token: option || undefined })
+                  }
+                  options={options}
+                />
+              </div>
+              <div className={styles.inputWrapper}>
+                <Input
+                  label="USDT ($)"
+                  type="number"
+                  id="usdt"
+                  value={formState.usdt}
+                />
+              </div>
+              <div className={styles.inputWrapper}>
+                <Input
+                  label="Amount"
+                  type="number"
+                  id="amount"
+                  value={formState.amount}
+                />
+              </div>
+              <div className={styles.inputWrapper}>
+                <Input
+                  label="Price"
+                  type="number"
+                  id="price"
+                  value={formState.price}
+                />
+              </div>
+            </form>
+            <Button
+              cssProps={{
+                height: "60px",
+                background: "#FED521",
+                fontSize: "24px",
+                fontWeight: 600,
+                color: "#292929",
+                boxShadow:
+                  "inset 0px -4px 2px #feb321, 0px 2px 2px rgba(169, 0,0,0.5)",
+                marginTop: 32,
+              }}
+              disabled={!price || !amount || !token?.value || !usdt}
+              onClick={handleAddToBagClick}
+            >
+              Add To Bag
+            </Button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,6 +1,18 @@
 import { create } from "zustand";
 import WebApp from "@twa-dev/sdk";
 
+interface Transaction {
+  usdt: string;
+  amount: string;
+  price: string;
+  date: string;
+  type: "buy" | "sell";
+}
+
+interface Token {
+  transactionsHistory: Transaction[];
+}
+
 interface UserStore {
   isTmaInfoLoading: boolean;
   isUserLoading: boolean;
@@ -8,16 +20,67 @@ interface UserStore {
   fetchUserAccount: (chatId: string) => Promise<void>;
   setUserTmaInfo: (userTmaInfo: typeof WebApp.initDataUnsafe) => void;
   userAccount?: {
-    tokens: string[];
+    tokens: {
+      [tokenId: string]: Token;
+    };
   };
+  buyToken: ({
+    tokenId,
+    date,
+    usdt,
+    amount,
+    price,
+  }: {
+    tokenId: string;
+    usdt: string;
+    amount: string;
+    price: string;
+    date: string;
+  }) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
-  userAccount: undefined,
+  userAccount: {
+    tokens: {},
+  },
   isUserLoading: true,
   isTmaInfoLoading: true,
   userTmaInfo: undefined,
-  fetchUserAccount: async (chatId: string) => {},
+  buyToken: ({ tokenId, date, usdt, amount, price }) => {
+    set((state) => {
+      if (!state.userAccount) {
+        return state;
+      }
+
+      const newTransaction: Transaction = {
+        usdt,
+        amount,
+        price,
+        date,
+        type: "buy",
+      };
+
+      const updatedTokens = { ...state.userAccount.tokens };
+      if (updatedTokens[tokenId]) {
+        updatedTokens[tokenId].transactionsHistory.push(newTransaction);
+      } else {
+        updatedTokens[tokenId] = {
+          transactionsHistory: [newTransaction],
+        };
+      }
+
+      return {
+        ...state,
+        userAccount: {
+          ...state.userAccount,
+          tokens: updatedTokens,
+        },
+      };
+    });
+  },
+  fetchUserAccount: async (chatId: string) => {
+    // Implementation for fetching user account
+  },
   setUserTmaInfo: (userTmaInfo: typeof WebApp.initDataUnsafe) => {
     set({ userTmaInfo, isTmaInfoLoading: false });
   },
